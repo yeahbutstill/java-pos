c6bb73504dcb14e3c8b35d19da81d24114dd9471
Dani Setiawan <mayadani28@gmail.com>
2022-01-07 03:58:17 +0700

    constant

---
+++ src/main/java/intro/Constants.java
@@ -0,0 +1,8 @@
+package intro;
+
+public interface Constants {
+  Integer MAX_USER = 10;
+  String APPLICATION_NAME = "POS";
+  String LAKI_LAKI = "L";
+  String PEREMPUAN = "P";
+}
---
+++ src/main/java/intro/ConstantsClass.java
@@ -0,0 +1,8 @@
+package intro;
+
+public class ConstantsClass {
+  public static final Integer MAX_USER = 10;
+  public static final String APPLICATION = "POS";
+  public static final String LAKI_LAKI = "L";
+  public static final String PEREMPUAN = "P";
+}
